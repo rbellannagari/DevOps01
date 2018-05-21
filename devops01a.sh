fa606a7e6ccca0a1cafbde817b1ae3cadf21d890
Raja Bellannagari <rbellannagari@Rajas-MacBook-Pro.local>
2018-05-21 09:57:55 -0700

    update

---
+++ devops01a.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "==========Volumes=================================="
+df -h
+
+echo "==========Number of Cpu/Cores======================="
+#sysctl -n hw.logicalcpu hw.physicalcpu machdep.cpu.brand_string
+system_profiler SPHardwareDataType | grep 'Processor Name:\|Total Number of Cores'
+
+echo "==========Amount of RAM============================="
+#sysctl -h hw.memsize
+system_profiler SPHardwareDataType | grep 'Memory'
+
+echo "==========Your MAC Address and IP Addresss=========="
+
+ifconfig en1 | grep -w 'ether\|inet'
