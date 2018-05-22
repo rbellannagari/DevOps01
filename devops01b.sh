c25b967b896ef69198b9283b8ba2604e5e1dc2d0
Raja Bellannagari <rbellannagari@gmail.com>
2018-05-21 20:59:01 -0700

    new update

--- devops01b.sh
+++ devops01b.sh
@@ -1,32 +1,31 @@
 #!/bin/sh
 # This is checking cronjob
 
-func01(){
-
-if [ -n "$1" ]
-
-echo "volumes""
-df -h
-
-if [ -n "$2" ]
-echo "Cpu"
-system_profiler SPHardwareDataType | grep 'Processor Name:\|Total Number of Cores'
-
-if [ -n "$3" ]
-echo "RAM"
-system_profiler SPHardwareDataType | grep 'Memory'
-
-if [ -n "$4" ]
-echo "Network"
-ifconfig en1 | grep -w 'ether\|inet'
-
-if [ -n "$@" ]
+if [ $1 == 'volumes' ]
+then
+  echo "volumes""
+  df -h
+elif [ $2 == 'cpu' ]
+then
+  echo "cpu"
+  system_profiler SPHardwareDataType | grep 'Processor Name:\|Total Number of Cores'
+elif [ $3 == 'ram' ]
+then
+  echo "ram"
+  system_profiler SPHardwareDataType | grep 'Memory'
+elif [ $4 == 'network' ]
+then
+  echo "Network"
+  ifconfig en1 | grep -w 'ether\|inet'
+elif [ $5 == 'all' ]
+then
+  echo "all"
+  df -h
+  system_profiler SPHardwareDataType | grep 'Processor Name:\|Total Number of Cores'
+  system_profiler SPHardwareDataType | grep 'Memory'
+  ifconfig en1 | grep -w 'ether\|inet'
+else
+  echo "some thing wrong"
 
 fi
-fi
-fi
-fi
-fi
-
-}
 
