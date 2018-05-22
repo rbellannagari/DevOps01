f47c256d6a7dacd3a7b17caf61b70e1b8b45a858
Raja Bellannagari <rbellannagari@gmail.com>
2018-05-21 22:59:01 -0700

    new update

--- devops01b.sh
+++ devops01b.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
 # This is checking cronjob
 
-if [ $1 == 'volumes' ]
-then
-  echo "volumes""
-  df -h
-elif [ $2 == 'cpu' ]
-then
-  echo "cpu"
-  system_profiler SPHardwareDataType | grep 'Processor Name:\|Total Number of Cores'
-elif [ $3 == 'ram' ]
-then
-  echo "ram"
-  system_profiler SPHardwareDataType | grep 'Memory'
-elif [ $4 == 'network' ]
-then
-  echo "Network"
-  ifconfig en1 | grep -w 'ether\|inet'
-elif [ $5 == 'all' ]
-then
-  echo "all"
-  df -h
-  system_profiler SPHardwareDataType | grep 'Processor Name:\|Total Number of Cores'
-  system_profiler SPHardwareDataType | grep 'Memory'
-  ifconfig en1 | grep -w 'ether\|inet'
-else
-  echo "some thing wrong"
-
-fi
-
+case "$*" in
+     volumes)
+          echo "volumes"
+          df -h
+          ;;
+     cpu)
+          echo "cpu"
+          system_profiler SPHardwareDataType | grep 'Processor Name:\|Total Number of Cores'
+          ;;
+     ram)
+          echo "ram"
+          system_profiler SPHardwareDataType | grep 'Memory'
+          ;;
+     network)
+          echo "Network"
+          ifconfig en1 | grep -w 'ether\|inet'
+          ;;
+     all)
+          echo "all"
+          df -h
+          system_profiler SPHardwareDataType | grep 'Processor Name:\|Total Number of Cores'
+          system_profiler SPHardwareDataType | grep 'Memory'
+          ifconfig en1 | grep -w 'ether\|inet'
+          ;;
+     *)
+          echo "Provide Options like devops01b.sh volumes || devops01b.sh cp || devops01b.sh cpu || devops01b.sh network || devops01b.sh all"
+          ;;
+esac
